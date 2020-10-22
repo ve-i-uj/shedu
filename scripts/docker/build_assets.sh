65741505b815442636ab4d6082af9b980515a39d
Aleksei Burov <a.burov@mednote.life>
2020-10-22 12:15:04 +0500

    A new script for building a docker image contained kbe assets

---
+++ scripts/docker/build_assets.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# Build a docker image of KBEngine with assets
+#
+
+# import global variables of scripts
+curr_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+source `realpath $curr_dir/../init.sh`
+curr_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+source `realpath $curr_dir/init.sh`
+
+echo 
+echo "DOCKERFILES_DIR=$DOCKERFILES_DIR"
+echo
+
+from=$COMPILED_IMAGE_NAME
+echo
+echo "*** Build an image contained compiled KBEngine (from '$from') ***"
+echo
+cd $DOCKERFILES_DIR/kbengine/$ASSETS_DIR
+docker build --build-arg FROM_IMAGE_NAME=$from -t $ASSETS_IMAGE_NAME:latest .
+
+# TODO: version of assets in a tag
+
