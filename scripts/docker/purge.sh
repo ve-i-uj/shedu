32188ed68ccbc5a1b4a56e40247a7cddae0b0bf2
Aleksei Burov <burov_alexey@mail.com>
2021-09-28 07:10:59 +0500

    Scripts to delete all project containers and volumes.

---
+++ scripts/docker/purge.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+# Delete all containers and volumes
+#
+
+
+# import global variables of scripts
+curr_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+source `realpath $curr_dir/init.sh`
+
+docker-compose --env-file "$PROJECT_DIR/configs/dev.env" down -v
+
