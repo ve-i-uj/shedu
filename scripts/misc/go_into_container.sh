7cf65fa86cb5efe7b570b8ec001e8dac8f94e133
Aleksei Burov <burov_alexey@mail.com>
2021-11-03 19:32:39 +0500

    Added a new script to check the game container is running.

--- scripts/misc/go_into_container.sh
+++ scripts/misc/go_into_container.sh
@@ -10,17 +10,9 @@ source $( realpath "$curr_dir"/../init.sh )
 # to create a game container on start. Stopped game container can exist
 # like the result of unexpected stopping but an assets container is one-off
 # and it will be deleted on the next start.
-
-assets_image=$( bash "$curr_dir/list_images.sh" | grep "$ASSETS_IMAGE_NAME" | head -n 1 )
-if [ -z "$assets_image" ]; then
-    echo "[ERROR] No assets image. Build the game image at first"
+bash "$curr_dir/is_running.sh"
+if [ "$?" -ne 0 ]; then
     exit 1
 fi
 
-container_id=$( docker ps --filter name=kbe-assets --filter status=running -q )
-if [ -z "$container_id" ]; then
-    echo "[ERROR] No running game container. Start the game at first"
-    exit 1
-fi
-
-docker exec --interactive --tty "$container_id" /bin/bash
+docker exec --interactive --tty "$CONTAINER_NAME" /bin/bash
