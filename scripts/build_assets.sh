f7821d851e4e92be71c5e4de056275d1e9c91efa
Aleksei Burov <burov.alexey1987@gmail.com>
2022-07-22 13:02:29 +0500

    The build_assets.sh can download kbe assets demo

--- scripts/build_assets.sh
+++ scripts/build_assets.sh
@@ -15,8 +15,10 @@ bash $0 \\
   --assets-version=v0.0.1
 "
 
+echo -e "*** Build a docker image of compiled KBEngine with assets ***"
+
 echo "Parse CLI arguments ..."
-version=""
+kbe_version=""
 assets_path=""
 assets_version=""
 help=false
@@ -25,7 +27,7 @@ do
     key=$( echo "$arg" | cut -f1 -d= )
     value=$( echo "$arg" | cut -f2 -d= )
     case "$key" in
-        --kbe-version)  version=${value} ;;
+        --kbe-version)  kbe_version=${value} ;;
         --assets-path)  assets_path=${value} ;;
         --assets-version)   assets_version=${value} ;;
         --help)         help=true ;;
@@ -34,15 +36,29 @@ do
     esac
 done
 
+if [ "$help" = true ]; then
+    echo -e "$USAGE"
+    exit 0
+fi
+
 echo "CLI arguments: "
-echo "    --kbe-version=$version"
+echo "    --kbe-version=$kbe_version"
 echo "    --assets-path=$assets_path"
 echo "    --assets-version=$assets_version"
 
-if [ -z "$version" ] || [ -z "$assets_path" ] || [ -z "$assets_version" ] || [ "$help" = true ]; then
+if [ -z "$kbe_version" ] || [ -z "$assets_path" ] || [ -z "$assets_version" ]; then
     echo "[ERROR] Not all arguments passed" >&2
     echo -e "$USAGE"
     exit 1
+fi
+
+if [ "$assets_path" == "demo" ]; then
+    echo "[INFO] The game will be created based on demo assets"
+    assets_path=/tmp/kbe-demo-assets
+    if [ -d "$assets_path" ]; then
+        rm -rf /tmp/kbe-demo-assets
+    fi
+    git clone "$ASSETS_DEMO_URL" $assets_path
 fi
 
 if [ ! -d "$assets_path" ]; then
@@ -51,12 +67,12 @@ if [ ! -d "$assets_path" ]; then
     exit 1
 fi
 
-sha=$( echo "$version" | cut -d '-' -f 1 )
+sha=$( echo "$kbe_version" | cut -d '-' -f 1 )
 echo "The KBEngine commit \"$sha\" is needed. Checking the docker image of compiled KBE (this version) exists ..."
 existed=$( docker images --format "{{.Repository}}:{{.Tag}}" \
-    | grep "$PRE_ASSETS_IMAGE_NAME:$version" )
+    | grep "$PRE_ASSETS_IMAGE_NAME:$kbe_version" )
 if [ -z "$existed"  ]; then
-    echo -e "[ERROR] There is NO compiled KBEngine with the version \"$version\". Build compiled kbe at first." >&2
+    echo -e "[ERROR] There is NO compiled KBEngine with the version \"$kbe_version\". Build compiled kbe at first." >&2
     choice_set=$( docker images --format "{{.Tag}} ({{.Repository}}:{{.Tag}})" | grep "$PRE_ASSETS_IMAGE_NAME" )
     echo -e "\nAvailable kbe versions:\n$choice_set"
     echo "$USAGE"
@@ -67,8 +83,8 @@ echo "The compiled KBEngine image exists"
 cd "$assets_path"
 docker build \
     --file "$ASSETS_DOCKERFILE_PATH" \
-    --build-arg FROM_IMAGE_NAME="$PRE_ASSETS_IMAGE_NAME:$version" \
-    --tag "$ASSETS_IMAGE_NAME-$version:$assets_version" \
+    --build-arg FROM_IMAGE_NAME="$PRE_ASSETS_IMAGE_NAME:$kbe_version" \
+    --tag "$ASSETS_IMAGE_NAME-$kbe_version:$assets_version" \
     .
 
 echo "Done ($0)"
