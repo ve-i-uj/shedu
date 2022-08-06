3a6c7448dc1298ff09388f58c621a30cedfd1099
Aleksei Burov <burov.alexey1987@gmail.com>
2022-08-06 18:22:48 +0500

    DB setting in the "modify_kbe_config.py" through the cli arguments + logging
    The two parameters the script needs to know are assets and settings path.

--- deploy-scripts/modify_kbe_config.py
+++ deploy-scripts/modify_kbe_config.py
@@ -3,46 +3,110 @@
 It would be placed in the root directory of the assets.
 """
 
-import os
+import argparse
+import logging
+import shutil
+import sys
 import xml.etree.ElementTree as ET
+from pathlib import Path
 
 HOST_ADDR = '0.0.0.0'
+TITLE = ('The script modifies the kbengine.xml configuration file so KBEngine '
+         'can work with docker.')
+FORMAT = '%(levelname)-7s %(asctime)s [%(filename)s:%(lineno)s - %(funcName)s()] %(message)s'
 
-MYSQL_DATABASE = os.environ['MYSQL_DATABASE']
-MYSQL_USER = os.environ['MYSQL_USER']
-MYSQL_PASSWORD = os.environ['MYSQL_PASSWORD']
-
-# TODO: [30.07.2022 burov_alexey@mail.ru]:
-# 1. Общий перехватчик ошибки?
-# 2. Запиусть под дебагом
-# 3. Смотреть под дебагом xml, которую удаляю
+logger = logging.getLogger(__file__)
 
 
+def read_args():
+    parser = argparse.ArgumentParser(description=TITLE)
+    parser.add_argument('--kbe-assets-path', dest='kbe_assets_path', type=str,
+                        required=True,
+                        help='The path to the game assets')
+    parser.add_argument('--env-file', dest='env_file_path', type=str,
+                        required=True,
+                        help='Settings file')
+    parser.add_argument('--log-level', dest='log_level', type=str,
+                        default='DEBUG',
+                        choices=logging._nameToLevel.keys(),
+                        help='Settings file')
+
+    return parser.parse_args()
+
+
+def setup_root_logger(level_name: str):
+    level = logging.getLevelName(level_name)
+    logger = logging.getLogger()
+    logger.setLevel(level)
+    stream_handler = logging.StreamHandler(sys.stdout)
+    formatter = logging.Formatter(FORMAT)
+    stream_handler.setFormatter(formatter)
+    logger.handlers = [stream_handler]
+    logger.info(f'Logger was set (log level = "{level_name}")')
+
+
+def check_settings(settings: dict) -> bool:
+    """Check settings file."""
+    res = True
+    for name, value in settings.items():
+        value = value.strip()
+        if name == 'MYSQL_DATABASE' and not value:
+            logger.error('The variable "MYSQL_DATABASE" is empty')
+            res = False
+        if name == 'MYSQL_USER' and not value:
+            logger.error('The variable "MYSQL_USER" is empty')
+            res = False
+        if name == 'MYSQL_PASSWORD' and not value:
+            logger.error('The variable "MYSQL_PASSWORD" is empty')
+            res = False
+
+    return res
 
 
 def main():
-    curr_dir = os.path.dirname(__file__)
-    conf_path = os.path.join(curr_dir, 'res', 'server', 'kbengine.xml')
-    # TODO: [26.10.2021 burov_alexey@mail.ru]:
-    # Проверка, что путь существует иначе запись в лог и выход с ошибкой (exit 1)
-    tree = ET.parse(conf_path)
+    namespace = read_args()
+    setup_root_logger(level_name=namespace.log_level)
+    kbengine_xml_path = Path(namespace.kbe_assets_path) / 'res' / 'server' / 'kbengine.xml'
+    if not kbengine_xml_path.exists():
+        logger.error('There is no kbengine.xml by path "%s"', kbengine_xml_path)
+        sys.exit(1)
+
+    settings_path = Path(namespace.env_file_path)
+    if not settings_path.exists():
+        logger.error('There is no settings env file by path "%s"', settings_path)
+        sys.exit(1)
+
+    settings: dict[str, str] = {}
+    for line in settings_path.open():
+        if not line.strip() or line.strip().startswith('#'):
+            continue
+        var_name, value = line.split('=', 1)
+        if not var_name:
+            logger.warning('A strange line is in config file ("%s")', line)
+            continue
+        settings[var_name] = value
+
+    if not check_settings(settings):
+        sys.exit(1)
+
+    logger.info('Copy origin "kbengine.xml" (to "kbengine.xml.bak") ')
+    shutil.copyfile(kbengine_xml_path,
+                    kbengine_xml_path.with_suffix(kbengine_xml_path.suffix + '.bak'))
+
+    tree = ET.parse(kbengine_xml_path)
     root = tree.getroot()
 
-    databaseInterface_el = root.find('dbmgr/databaseInterfaces')  # noqa
-    default_el = root.find('dbmgr/databaseInterfaces/default')
-    # TODO: [26.10.2021 burov_alexey@mail.ru]:
-    # Элемент нужно обновлять, а не удалять. Другие настройки должны остаться
-    # не тронутыми.
-    databaseInterface_el.remove(default_el)
+    dbmgr_el = root.find('dbmgr')
+    if dbmgr_el is None:
+        dbmgr_el = ET.SubElement(root, 'dbmgr')
 
-#     print(
-#         f'MYSQL_USER={MYSQL_USER}',
-#         f'MYSQL_PASSWORD={MYSQL_PASSWORD}',
-#         f'MYSQL_DATABASE={MYSQL_DATABASE}',
-#         sep='\n'
-#     )
+    databaseInterface_el = dbmgr_el.find('databaseInterfaces')  # noqa
+    if databaseInterface_el is None:
+        databaseInterface_el = ET.SubElement(dbmgr_el, 'databaseInterfaces')
 
-    assert MYSQL_USER and MYSQL_PASSWORD and MYSQL_DATABASE
+    default_el = databaseInterface_el.find('default')
+    if default_el is None:
+        default_el = ET.SubElement(databaseInterface_el, 'default')
 
     new_default_el = ET.fromstring(f"""
         <default>
@@ -50,29 +114,44 @@ def main():
             <host> kbe-mariadb </host>
             <port> 0 </port>
             <auth>
-                <username> {MYSQL_USER} </username>
-                <password> {MYSQL_PASSWORD} </password>
+                <username> {settings['MYSQL_USER']} </username>
+                <password> {settings['MYSQL_PASSWORD']} </password>
             </auth>
-            <databaseName> {MYSQL_DATABASE} </databaseName>
+            <databaseName> {settings['MYSQL_DATABASE']} </databaseName>
         </default>
     """)
 
-#   print(ET.tostring(new_default_el, encoding='utf8', method='xml'))
+    to_update_els = [el for el in new_default_el if el is not new_default_el]
+    for new_el in to_update_els:
+        el = default_el.find(new_el.tag)
+        if el is not None:
+            default_el.remove(el)
+        default_el.append(new_el)
 
-    databaseInterface_el.append(new_default_el)
+    logger.info('Updated elements: {tags})'.format(
+        tags=', '.join(f'"{el.tag}"' for el in to_update_els))
+    )
 
-    dbmgr_el = root.find('dbmgr')
-    # TODO: (3 nov. 2020 г. 12:35:19 burov_alexey@mail.ru)
-    # Check node exists
-    shareDB_el = ET.SubElement(dbmgr_el, 'shareDB')  # noqa
+    shareDB_el = dbmgr_el.find('shareDB')
+    if shareDB_el is None:
+        shareDB_el = ET.SubElement(dbmgr_el, 'shareDB')
     shareDB_el.text = 'true'
+
+    logger.info('Updated "shareDB"')
 
     for name in ('baseapp', 'loginapp'):
         app_el = root.find(name)
-        externalAddress_el = ET.SubElement(app_el, 'externalAddress')  # noqa
+        if app_el is None:
+            app_el = ET.SubElement(root, name)
+
+        externalAddress_el = app_el.find('externalAddress')
+        if externalAddress_el is None:
+            externalAddress_el = ET.SubElement(app_el, 'externalAddress')
         externalAddress_el.text = HOST_ADDR
 
-    tree.write(conf_path)
+        logger.info(f'Updated {name} "externalAddress"')
+
+    tree.write(kbengine_xml_path.with_suffix(kbengine_xml_path.suffix + '.new'))
 
     print('The config "kbengine.xml" has been updated')
 
