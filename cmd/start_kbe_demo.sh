66e67085559fcba9d55de34513abf2f88b3bb6f9
Aleksei Burov <burov_alexey@mail.ru>
2022-09-25 05:53:39 +0500

    Executable mode for all bash scripts was added

---
+++ cmd/start_kbe_demo.sh
@@ -0,0 +1,10 @@
+curr_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+source $( realpath "$curr_dir/../scripts/init.sh" )
+
+cd "$PROJECT_DIR"
+
+make stop
+cp configs/kbe-v2.5.12-demo.env .env
+make build_game
+make start
+make logs
