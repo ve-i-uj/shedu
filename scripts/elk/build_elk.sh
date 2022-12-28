4125294d45076b6681a4ddf45aee9d14df370c13
Aleksei Burov <burov_alexey@mail.ru>
2022-12-28 07:56:48 +0500

    Using the "Dejavu" ES client to view logs

--- scripts/elk/build_elk.sh
+++ scripts/elk/build_elk.sh
@@ -6,8 +6,9 @@ set -e
 curr_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source "$( realpath "$curr_dir/../init.sh" )"
 
-bash "$curr_dir/es/build_es.sh"
+bash "$curr_dir/elastic/build_elastic.sh"
 bash "$curr_dir/kibana/build_kibana.sh"
 bash "$curr_dir/logstash/build_logstash.sh"
+bash "$curr_dir/dejavu/build_dejavu.sh"
 
 echo -e "\nDone ($0)"
---
+++ scripts/elk/dejavu/build_dejavu.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Import global constants of the project
+curr_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+source "$( realpath "$curr_dir/../../init.sh" )"
+
+cd "$PROJECT_DIR"
+docker pull $ELK_DEJAVU_IMAGA_NAME
+docker tag $ELK_DEJAVU_IMAGA_NAME "$ELK_DEJAVU_IMAGE_TAG"
+echo "The image \"$ELK_DEJAVU_IMAGE_TAG\" have been created"
