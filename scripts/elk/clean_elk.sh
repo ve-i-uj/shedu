1e988002187c031a54b40e77f6773b1f5bd322af
Aleksei Burov <burov_alexey@mail.ru>
2022-12-29 09:23:37 +0500

    The "clean.sh" script deletes the ES volume

--- scripts/elk/clean_elk.sh
+++ scripts/elk/clean_elk.sh
@@ -2,26 +2,33 @@
 
 set -e
 
-# Import global constants of the project
-curr_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-source "$( realpath "$curr_dir/../init.sh" )"
-
-echo "[INFO] Stop containers"
-# docker ps --filter name="$ELK_C_NAME_PREFIX*" --filter status=running -aq \
-#     | xargs docker stop
 res=$( docker ps --filter name="$ELK_C_NAME_PREFIX*" -q )
 if [ ! -z $res ]; then
-    echo $res | xargs docker container rm
+    echo "[INFO] Delete containers"
+    echo $res | xargs docker container rm 1>/dev/null
 fi
 
 if [ -n "$( docker images --filter reference="$ELK_ES_IMAGE_TAG" -q )" ]; then
-    docker rmi $ELK_ES_IMAGE_TAG
+    echo "[INFO] Delete the ES tag"
+    docker rmi $ELK_ES_IMAGE_TAG 1>/dev/null
 fi
 if [ -n "$( docker images --filter reference="$ELK_KIBANA_IMAGE_TAG" -q )" ]; then
-    docker rmi $ELK_KIBANA_IMAGE_TAG
+    echo "[INFO] Delete the Kibana tag"
+    docker rmi $ELK_KIBANA_IMAGE_TAG 1>/dev/null
 fi
 if [ -n "$( docker images --filter reference="$ELK_LOGSTASH_IMAGE_TAG" -q )" ]; then
-    docker rmi $ELK_LOGSTASH_IMAGE_TAG
+    echo "[INFO] Delete the LogStash tag"
+    docker rmi $ELK_LOGSTASH_IMAGE_TAG 1>/dev/null
+fi
+if [ -n "$( docker images --filter reference="$ELK_DEJAVU_IMAGE_TAG" -q )" ]; then
+    echo "[INFO] Delete the Dejavu tag"
+    docker rmi $ELK_DEJAVU_IMAGE_TAG 1>/dev/null
+fi
+
+res=$( docker volume ls -f "name=$ELK_ES_VOLUME_NAME" -q )
+if [ ! -z $res ]; then
+    echo "[INFO] Delete the volume \"$res\""
+    docker volume rm "$res" 1>/dev/null
 fi
 
 echo -e "\nDone ($0)"
--- scripts/stop_game.sh
+++ scripts/stop_game.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 # Stop the game.
 
+# Import global constants of the project
+curr_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+source $( realpath "$curr_dir/init.sh" )
+
 # We don't need containers because the db has its own docker volume
 # and apps don't store state.
 echo "Delete containers ..."
