3962a68883ca2e9ec0816ec9906109783a7bafd3
Aleksei Burov <burov_alexey@mail.ru>
2022-12-29 09:22:40 +0500

    Fixed scripts building elk images

--- scripts/elk/elastic/build_elastic.sh
+++ scripts/elk/elastic/build_elastic.sh
@@ -2,7 +2,6 @@
 
 set -e
 
-cd "$PROJECT_DIR"
 docker pull $ELK_ES_IMAGE_NAME
 docker tag $ELK_ES_IMAGE_NAME "$ELK_ES_IMAGE_TAG"
 echo "The image \"$ELK_ES_IMAGE_TAG\" have been created"
--- scripts/elk/logstash/build_logstash.sh
+++ scripts/elk/logstash/build_logstash.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-cd "$PROJECT_DIR"
+set -e
+
 docker pull $ELK_LOGSTASH_IMAGA_NAME
 docker tag $ELK_LOGSTASH_IMAGA_NAME "$ELK_LOGSTASH_IMAGE_TAG"
 echo "The image \"$ELK_LOGSTASH_IMAGE_TAG\" have been created"
