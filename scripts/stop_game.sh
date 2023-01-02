3575646549dc4ea5480a6c8f478d21323b9fe3f0
Aleksei Burov <burov_alexey@mail.ru>
2023-01-02 16:54:43 +0500

    Pushing unparsed messages to the "failure" index

--- scripts/stop_game.sh
+++ scripts/stop_game.sh
@@ -5,7 +5,5 @@
 curr_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source $( realpath "$curr_dir/init.sh" )
 
-# We don't need containers because the db has its own docker volume
-# and apps don't store state.
-echo "Delete containers ..."
-docker-compose rm -fsv
+docker-compose stop kbe-assets 2>/dev/null
+docker-compose stop kbe-mariadb 2>/dev/null
